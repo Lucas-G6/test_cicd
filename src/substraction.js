088d0ac70ad06e8ec7b01319912014fcf80981e1
Svenor <lucas.grospart@gmail.com>
2022-06-16 15:05:14 +0200

    1st test de pipeline

---
+++ src/substraction.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @param {int} a First number
+ * @param {number} b Number to substract
+ * @returns The substraction
+ */
+function substraction(a, b) {
+    return a - b;
+}
+
+module.exports = substraction;
---
+++ tests/substraction.test.js
@@ -0,0 +1,6 @@
+const substraction = require("../src/substraction");
+
+test("substracts 4 - 2 to equal 2", () => {
+   expect(substraction(4, 2)).toBe(2);
+});
+
